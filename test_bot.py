db131d683a787db27c225dc4d51cdbeebef3ab1d
Somesh Bhosale <someshbhosale2@gmail.com>
2025-03-22 01:18:27 +0530

    Bot

--- test_bot.py
+++ test_bot.py
@@ -18,17 +18,13 @@ CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL"))  # Check every 10 seconds
 
 # ------------------- STEP 2: Set Up Selenium -------------------
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run headless
+chrome_options.add_argument("--headless")  # Run in headless mode
 chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--window-size=1920,1080")
+# chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Use Railway's Chromium Path
-chrome_options.binary_location = "/usr/bin/chromium"
-
-# Set WebDriver Path
-service = Service("/usr/bin/chromedriver")
+service = Service(ChromeDriverManager().install())
 
 def fetch_matches():
     """Scrapes match data from the website and returns a list of matches."""
